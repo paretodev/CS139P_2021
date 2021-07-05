2e513185f687284e66de90bc0b98ae75b6b24307
dylan <dylan@dunamu.com>
2021-07-05 12:13:02 +0900

    Day5 endpoint

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -15,53 +15,16 @@ struct ContentView: View {
         VStack {
             ScrollView{
                 LazyVGrid( columns: [ GridItem( .adaptive(minimum: 65.0) ) ] ) {
-                    /*
-                    it's lazy about accessing "the body vars" of its views.
-                    in one row, put as many cards as possible, with min. width of 65.0pt.
-                    - a row, a column 에 대한 description이 담긴 객체
-                    */
                     ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
                         CardView(content: emoji).aspectRatio(2/3, contentMode : .fit)
                     }
                 }
                 .foregroundColor(.red)
             }
-            
-            Spacer()
-            
-            HStack{
-                add
-                Spacer()
-                remove
-            }
-            .font(.largeTitle)
-            .padding(.horizontal)
         }
         .padding(.all)
     }
     
-    //MARK:- + Variables
-    var remove : some View {
-        Button{
-            if emojiCount > 1 {
-                emojiCount -= 1
-            }
-        } label: {
-            Image(systemName: "minus.circle")
-        }
-    }
-    
-    var add : some View {
-        
-        Button {
-            if emojiCount < emojis.count {
-                emojiCount += 1
-            }
-        } label: {
-            Image(systemName: "plus.circle")
-        }
-        
-    }
 }
 
 //MARK- Types
@@ -69,7 +32,7 @@ struct CardView : View {
     var content : String
     @State var isFaceUp : Bool = true
     
-    // system accesses a view's var body, only when it becomes necessary ex). View will appear on the screen soon
+    //lazy access if i use lazy vertical grid
     var body : some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20.0)
---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,18 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Dylan Han on 2021/07/05.
+//
+
+import Foundation // <=> not UI job so...
+
+struct MemoryGame {
+    
+    
+    
+    
+    
+    
+    
+}
